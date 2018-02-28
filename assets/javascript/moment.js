c6d82bef44fd5a0fdac6b552f06a583f78126776
Samantha McCall <samanthaadams.biz@gmail.com>
2018-02-28 13:16:09 -0600

    creating site files

---
+++ assets/javascript/moment.js
@@ -0,0 +1,15 @@
+var config = {
+    apiKey: "AIzaSyC4d8tUlOfpW2ElJySU4UmuyCIl84PA3rI",
+    authDomain: "mariposadb-634.firebaseapp.com",
+    databaseURL: "https://mariposadb-634.firebaseio.com",
+    projectId: "mariposadb-634",
+    storageBucket: "mariposadb-634.appspot.com",
+    messagingSenderId: "1085144320325"
+  };
+  
+  firebase.initializeApp(config);
+  
+  // Create a variable to reference the database.
+  var database = firebase.database();
+
+  
